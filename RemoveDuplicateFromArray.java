4a229d6d0b70ca859f93d83061196e5bb335069d
pradipspidy <99903227+pradipspidy@users.noreply.github.com>
2025-08-16 12:16:00 +0530

    Create RemoveDuplicateFromArray.java

---
+++ RemoveDuplicateFromArray.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,9};
+        int n = arr.length;
+        Set<Integer> unique = new HashSet<>();
+        for(int i=0; i<n; i++){
+            unique.add(arr[i]);
+        }
+        System.out.println("unique item in array"+ unique.size());
+        int k = 0;
+        for(int item : unique){
+            arr[k] = item;
+            k++;
+        }
+        for(int j=k; j<n; j++){
+            arr[j] = 0;
+        }
+        
+        for(int j=0; j<n; j++){
+            System.out.println(arr[j]);
+        }
+        
+    }
+}
