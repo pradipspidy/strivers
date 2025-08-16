a094fe0e92d342e77140028bba56289126a8d673
pradipspidy <99903227+pradipspidy@users.noreply.github.com>
2025-08-16 12:29:34 +0530

    Update RemoveDuplicateFromArray.java

--- RemoveDuplicateFromArray.java
+++ RemoveDuplicateFromArray.java
@@ -1,46 +1,22 @@
 //time TC Worst/BEST = O(N), SC = O(1)
-import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,9};
-        int n = arr.length;
-        Set<Integer> unique = new HashSet<>();
-        for(int i=0; i<n; i++){
-            unique.add(arr[i]);
-        }
-        System.out.println("unique item in array"+ unique.size());
-        int k = 0;
-        for(int item : unique){
-            arr[k] = item;
-            k++;
-        }
-        for(int j=k; j<n; j++){
-            arr[j] = 0;
-        }
-        
-        for(int j=0; j<n; j++){
-            System.out.println(arr[j]);
-        }
-        
-    }
-}
-
-
-// // Online Java Compiler
-// // Use this editor to write, compile and run your Java code online
 // import java.util.*;
 // class Main {
 //     public static void main(String[] args) {
-//         int arr[] = {0,0,3,3,3,5,6};
+//         int arr[] = {1,2,3,4,5,6,9};
 //         int n = arr.length;
-//         int dup = 1;
-//         for(int i=1; i<n; i++){
-//            if(arr[i]!=arr[dup-1]){
-//                arr[dup] = arr[i];
-//                dup++;
-//            }
+//         Set<Integer> unique = new HashSet<>();
+//         for(int i=0; i<n; i++){
+//             unique.add(arr[i]);
 //         }
-//         System.out.println("unique item in array "+dup);
+//         System.out.println("unique item in array"+ unique.size());
+//         int k = 0;
+//         for(int item : unique){
+//             arr[k] = item;
+//             k++;
+//         }
+//         for(int j=k; j<n; j++){
+//             arr[j] = 0;
+//         }
         
 //         for(int j=0; j<n; j++){
 //             System.out.println(arr[j]);
@@ -48,3 +24,27 @@ class Main {
         
 //     }
 // }
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int arr[] = {0,0,3,3,3,5,6};
+        int n = arr.length;
+        int dup = 1;
+        for(int i=1; i<n; i++){
+           if(arr[i]!=arr[dup-1]){
+               arr[dup] = arr[i];
+               dup++;
+           }
+        }
+        System.out.println("unique item in array "+dup);
+        
+        for(int j=0; j<n; j++){
+            System.out.println(arr[j]);
+        }
+        
+    }
+}
