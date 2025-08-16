7cc60dd2810d11287746ff6c4829c12975996c9a
pradipspidy <99903227+pradipspidy@users.noreply.github.com>
2025-08-16 19:17:41 +0530

    Create MaximumConsecutiveOnes.java

---
+++ MaximumConsecutiveOnes.java
@@ -0,0 +1,23 @@
+//Linear search
+// TC O(n)
+// SP WC::O(1)
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+         int arr[] = {1, 1, 0, 0, 1, 1, 1, 1};
+         int mx =0;
+         int sum = 0;
+         for (int i = 0; i<arr.length; i++){
+             if(arr[i]!=0)
+             {
+                 sum +=1;
+             }else{
+                mx = Math.max(mx,sum);
+                sum=0;
+             }
+         }
+         mx = Math.max(mx, sum);
+         System.out.println("missingNumber:: "+ mx);
+    }
+    
+}
