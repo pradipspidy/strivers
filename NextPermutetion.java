418e08c0e2499deb66f6221682405e361a9927b4
pradipspidy <99903227+pradipspidy@users.noreply.github.com>
2025-08-18 01:42:00 +0530

    Create NextPermutetion.java

---
+++ NextPermutetion.java
@@ -0,0 +1,40 @@
+// Next permutation
+// TC O(N)
+// SP WC::O(N)
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4};
+        int n = arr.length;
+        int i = n-2;
+        while(i>=0 && arr[i]>=arr[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j = n-1;
+            while(j>=0 && arr[i]>=arr[j]){
+                j--;
+            }
+            swap(arr,i,j);
+        }
+         reverse(arr,i+1, n-1);
+         for(int k=0; k<n; k++){
+        System.out.println(arr[k]);
+        }
+}
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public static void reverse(int[] arr, int i, int j){
+        if(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;   
+        }
+    }
+    
+       
+}
